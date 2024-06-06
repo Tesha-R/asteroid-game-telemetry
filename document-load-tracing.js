c059f49fc574dbc66f918fa6de50685dc247ebd6
Tesha R <tesha@ldrdesigns.com>
2024-06-05 21:07:48 -0700

    Added registerInstrumentation

--- document-load-tracing.js
+++ document-load-tracing.js
@@ -1,6 +1,7 @@
 // document-load-tracing.js
 
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
 
 registerInstrumentations({
   instrumentations: [new DocumentLoadInstrumentation()],
